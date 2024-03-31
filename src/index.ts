c8d66be78c8cb914f861cb5b966c47d0387e08ae
Matheus Baraldi <matheushbaraldi@gmail.com>
2024-03-31 14:37:12 -0300

    feat: added swagger route

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ import app from "./app";
 import { PORT } from "./common/constants/constants";
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
+  console.log(
+    `Server is running on port http://localhost:${PORT}\nSwagger: http://localhost:${PORT}/api`,
+  );
 });
