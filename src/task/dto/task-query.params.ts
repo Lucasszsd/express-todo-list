9490512a6761648af279b891eeb87f882f674fe0
Matheus Baraldi <matheushbaraldi@gmail.com>
2024-03-31 23:46:14 -0300

    feat: added task-query params types

--- src/task/dto/task-query.params.ts
+++ src/task/dto/task-query.params.ts
@@ -3,15 +3,8 @@ import { Status } from "../entities/task.entity";
 export interface TaskQueryParams {
   longestDescription?: boolean;
   status?: Status;
-  userIdFilterType?: userIdFilterType;
   user_id?: string;
   category_id?: string;
   startConclusionDate?: string;
   endConclusionDate?: string;
-}
-
-enum userIdFilterType {
-  TASK_QUANTITY = "TASK_QUANTITY",
-  OLDEST_TASK = "OLDEST_TASK",
-  MOST_RECENT_TASK = "MOST_RECENT_TASK",
 }
